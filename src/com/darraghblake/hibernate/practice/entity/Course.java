3c43c46bea571b1ae89cf694545fb0defce6be6a
Darragh Blake <darragh.blake48@gmail.com>
2018-10-05 13:43:51 +0100

    Define Course Relationship

--- src/com/darraghblake/hibernate/practice/entity/Course.java
+++ src/com/darraghblake/hibernate/practice/entity/Course.java
@@ -1,6 +1,13 @@
 package com.darraghblake.hibernate.practice.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,10 +24,16 @@ public class Course {
 	
 	// annotate fields
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
 	
+	@Column(name="title")
 	private String title;
 	
+	@ManyToOne(cascade= {CascadeType.REFRESH, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
 	
 	public Course() {
